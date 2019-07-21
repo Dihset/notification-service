a3a268b17593eb82a753d07e669211436fe4452f
Dihset <slamihin123@gmail.com>
2019-07-21 22:01:28 +0400

    add timer task add send task

--- mailing-service/src/app.py
+++ mailing-service/src/app.py
@@ -1,20 +1,54 @@
-#import aiosmtplib
-#from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import aio_pika
+import aiosmtplib
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from .settings import *
 from .consumer import send_email_consumer
 
 
 async def init_app():
-    #smtp = await init_smtp()
-    #await send_email_consumer(smtp)
-    await send_email_consumer('dsgdsf')
+    smtp = await init_smtp()
+    queue = await init_queue()
+    scheduler = await init_scheduler()
+    await send_email_consumer(smtp, scheduler, queue)
 
 
+async def init_smtp():
+    smtp = aiosmtplib.SMTP(hostname=SMTP_HOSTNAME, port=SMTP_PORT)
+    await smtp.connect()
+    await smtp.starttls()
+    await smtp.login(SMTP_USERNAME, SMTP_PASSWORD)
+    return smtp
 
 
-#async def init_smtp():
-#    smtp = aiosmtplib.SMTP(hostname=SMTP_HOSTNAME, port=SMTP_PORT)
-#    await smtp.connect()
-#    await smtp.starttls()
-#    await smtp.login(SMTP_USERNAME, SMTP_PASSWORD)
-#    return smtp
+async def init_scheduler():
+    scheduler = AsyncIOScheduler()
+    scheduler.start()
+    return scheduler
+
+
+async def init_queue():
+    conn = await aio_pika.connect_robust(
+        'amqp://guest:guest@rabbitmq:5672/'
+    )
+    chan = await conn.channel()
+    await chan.set_qos(prefetch_count=1)
+    exchange = await chan.declare_exchange(
+        'test_incoming',
+        type=aio_pika.ExchangeType.DIRECT,
+        durable=True,
+    )
+    dlx_exchange = await chan.declare_exchange(
+        'dlx_incoming',
+        type=aio_pika.ExchangeType.DIRECT,
+        durable=True,
+    )
+    queue = await chan.declare_queue(
+        QUEUE_NAME,
+        durable=True,
+        arguments={
+            'x-message-ttl': 60000,
+            'x-dead-letter-exchange': dlx_exchange.name,
+        },
+    )
+    await queue.bind(exchange)
+    return queue
--- mailing-service/src/consumer.py
+++ mailing-service/src/consumer.py
@@ -1,45 +1,37 @@
 import logging
-import aio_pika
+import json
+import datetime
 from email.mime.text import MIMEText
-from .settings import SMTP_USERNAME, QUEUE_NAME
+from .settings import SMTP_USERNAME
 
 
-async def send_email_consumer(smtp):
-    conn = await aio_pika.connect_robust(
-        'amqp://guest:guest@rabbitmq:5672/'
-    )
-    chan = await conn.channel()
-    await chan.set_qos(prefetch_count=1)
-    exchange = await chan.declare_exchange(
-        'test_incoming',
-        type=aio_pika.ExchangeType.DIRECT,
-        durable=True,
-    )
-    dlx_exchange = await chan.declare_exchange(
-        'dlx_incoming',
-        type=aio_pika.ExchangeType.DIRECT,
-        durable=True,
-    )
-    queue = await chan.declare_queue(
-        QUEUE_NAME,
-        durable=True,
-        arguments={
-            'x-message-ttl': 60000,
-            'x-dead-letter-exchange': dlx_exchange.name,
-        },
-    )
-    await queue.bind(exchange)
+async def send_email_consumer(smtp, scheduler, queue):
     async with queue.iterator() as queue_iter:
         async for message in queue_iter:
             async with message.process():
-                logging.info(message.body)
+                email = parse_message(message)
+                scheduler.add_job(
+                    send_email,
+                    'date',
+                    run_date=email['date'],
+                    args=[smtp, email]
+                )
 
 
-
-async def send_email(smtp):
+async def send_email(smtp, data):
     message = MIMEText("Sent via aiosmtplib")
     message["From"] = SMTP_USERNAME
-    message["To"] = "zpm09119@bcaoo.com"
-    message["Subject"] = "Hello World!"
+    message["To"] = data['email']
+    message["Subject"] = data['text']
     await smtp.send_message(message)
-    print('OK')
+    logging.info('OK')
+
+
+def parse_message(message):
+    data = message.body.decode()
+    data = json.loads(data)
+    data['date'] = datetime.datetime.strptime(
+        data['date'],
+        '%Y-%m-%dT%H:%M:%S.%f'
+    )
+    return data
